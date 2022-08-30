8c3551f9fb193b0fc4e6d874cd039ac3813a9f28
CSG Team <csg@ucar.edu>
2022-08-29 20:49:21 -0600

    vanderwb: Publish new stack

---
+++ util/localinit.sh
@@ -0,0 +1,43 @@
+# Location variables
+export INSTALLPATH_ROOT=/glade/u/apps/gust/default/spack/opt/spack
+export MODULEPATH_ROOT=/glade/u/apps/gust/modules
+
+# Lmod configuration
+export LMOD_SYSTEM_NAME=gust
+export LMOD_SYSTEM_DEFAULT_MODULES="ncarenv/22.08b:craype/2.7.17:cce/14.0.2:ncarcompilers/0.6.2:cray-mpich/8.1.18:netcdf/4.8.1:cray-libsci/22.08.1.1"
+export MODULEPATH=/glade/u/apps/gust/modules/environment
+
+# Location of Lmod initialization scripts
+LMOD_ROOT=/glade/u/apps/gust/22.08b/spack/opt/spack/lmod/8.7.2/gcc/7.5.0
+
+# Use shell-specific init
+comm=`/bin/ps -p $$ -o cmd= |awk '{print $1}'|sed -e 's/-sh/csh/' -e 's/-csh/tcsh/' -e 's/-//g'`
+shell=`/bin/basename $comm`
+
+if [ -f $LMOD_ROOT/lmod/lmod/init/$shell ]; then
+    . $LMOD_ROOT/lmod/lmod/init/$shell
+else
+    . $LMOD_ROOT/lmod/lmod/init/sh
+fi
+
+unset comm shell
+
+# Load default modules
+if [ -z "$__Init_Default_Modules" -o -z "$LD_LIBRARY_PATH" ]; then
+  __Init_Default_Modules=1; export __Init_Default_Modules;
+  module -q restore 
+fi
+
+# Set system default stuff
+export PATH=${PATH}:/usr/local/bin:/usr/bin:/sbin:/bin
+export MANPATH=${MANPATH}:/usr/local/share/man:/usr/share/man
+export INFOPATH=${INFOPATH}:/usr/local/share/info:/usr/share/info
+
+# Set PBS workdir if appropriate
+if [ -n "$PBS_O_WORKDIR" ] && [ -z "$NCAR_PBS_JOBINIT" ]; then
+    if [ -d "$PBS_O_WORKDIR" ]; then
+        cd $PBS_O_WORKDIR
+    fi
+
+    export NCAR_PBS_JOBINIT=$PBS_JOBID
+fi
